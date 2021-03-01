382f7b71732f0106e932ea41d0e3469245c96bc9
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2021-03-01 09:46:14 -0300

    style: added the drawer to the home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, IconButton, Typography, Drawer, Button } from '@material-ui/core'
 import { Menu, GitHub } from '@material-ui/icons'
 
 export const Home = () => {
   const [component, setComponent] = useState('Professionals')
+  const [showDrawer, setShowDrawer] = useState(false)
+
+  function toggleDrawer() {
+    setShowDrawer(!showDrawer)
+  }
 
   return (
     <div>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
             <Menu />
           </IconButton>
           <Typography variant="h6">
             Componente exibido
           </Typography>
-          <IconButton>
-            <GitHub />
-          </IconButton>
+          <Button style={{ marginLeft: 'auto' }} color="inherit">Sobre</Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="left"
+        open={showDrawer}
+        onClose={toggleDrawer}
+      >
+        Something in the drawer
+      </Drawer>
     </div>
   )
 }
